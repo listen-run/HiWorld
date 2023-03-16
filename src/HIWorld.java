87117c7e2441e43700ccf814983c10e312418273
listen-run <3053343176@qq.com>
2023-03-16 16:26:46 +0800

    test1

--- src/HIWorld.java
+++ src/HIWorld.java
@@ -1,5 +1,12 @@
 public class HIWorld {
     public static void main(String[] args) {
+        boolean flag = true;
+        for (int i=0;i<5;i++){
+            if (flag){
+                System.out.println("0");
+            }
+            System.out.println("11");
+        }
         System.out.println("Hi World!");
     }
 }
